bb9df3c4f6612f38a774b740823fff01c1c998bd
Felipe Bulhões <felipebbulhoes@gmail.com>
2023-03-17 09:14:47 -0300

    feat: user permissions

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UserListCreateView, UserDetailView
+from .views import UserCreateView, UserDetailView, UserListView
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
-    path("users/", UserListCreateView.as_view(), name="user-list-create"),
-    path("login/", TokenObtainPairView.as_view()),
-    path("users/<str:user_id>/", UserDetailView.as_view()),
+    path("users/create/", UserCreateView.as_view(), name="user-create"),
+    path("users/list/", UserListView.as_view(), name="user-list"),
+    path("login/", TokenObtainPairView.as_view(), name="user-login"),
+    path("users/<str:user_id>/", UserDetailView.as_view(), name="user-detail"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,16 +1,34 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import (
+    RetrieveUpdateDestroyAPIView,
+    ListAPIView,
+    CreateAPIView,
+)
 from .serializers import UserSerializer
 from .models import User
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .permissions import UserDetailPermission
 
 
 # Create your views here.
-class UserListCreateView(ListCreateAPIView):
+class UserCreateView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserListView(ListAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
 class UserDetailView(RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, UserDetailPermission]
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
     lookup_url_kwarg = "user_id"
