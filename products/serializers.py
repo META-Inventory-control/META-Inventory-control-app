a37a5d525505920fcb0ef3e4afde844cc92e30d2
Felipe Bulhões <felipebbulhoes@gmail.com>
2023-03-17 09:49:16 -0300

    feat: products permissions

--- products/serializers.py
+++ products/serializers.py
@@ -7,6 +7,17 @@ class ProductSerialzier(serializers.ModelSerializer):
         read_only=True, max_digits=7, decimal_places=2
     )
 
+    def update(self, instance: Product, validated_data: dict):
+        entry_cost = validated_data.pop("entry_cost", None)
+        # Lógica do final price
+        if entry_cost:
+            setattr(instance, "entry_cost", entry_cost)
+            setattr(instance, "final_cost", entry_cost * 2)
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+        instance.save()
+        return instance
+
     class Meta:
         model = Product
         fields = "__all__"
