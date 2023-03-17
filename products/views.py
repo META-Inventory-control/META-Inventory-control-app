1828b3119fd0937add407131c43c5e4e019359a8
Felipe Bulhões <felipebbulhoes@gmail.com>
2023-03-16 22:26:09 -0300

    base products funcionality

--- products/views.py
+++ products/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import generics, parsers
+from .models import Product
+from .serializers import ProductSerialzier
 
 # Create your views here.
+
+
+class ProductView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerialzier
+
+    def perform_create(self, serializer):
+        # Lógica do preço final
+        final_cost = int(self.request.data["entry_cost"]) * 2
+        serializer.save(final_cost=final_cost)
+
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerialzier
+    lookup_url_kwarg = "product_id"
