2e55661590380c79407e5934379e3b0afbdfe4d0
Felipe Bulhões <felipebbulhoes@gmail.com>
2023-04-27 17:26:41 -0300

    fix: final cost maths and code generate logic

--- products/views.py
+++ products/views.py
@@ -21,10 +21,34 @@ class ProductView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         # Lógica do código
         productsList = Product.objects.all()
+        biggestCode = 0
+        for product in productsList:
+            if product.code > biggestCode:
+                biggestCode = product.code
 
         # Lógica do preço final
-        final_cost = float(self.request.data["entry_cost"]) * 2
-        serializer.save(final_cost=final_cost, code=f"MT{productsList.count() + 1}")
+        # final_cost = float(self.request.data["entry_cost"]) * 2
+        entry_cost = float(self.request.data["entry_cost"])
+
+        if entry_cost <= 50:
+            final_cost = entry_cost * 6
+        elif entry_cost > 50 and entry_cost <= 150:
+            final_cost = entry_cost * 3
+        elif entry_cost > 151 and entry_cost <= 700:
+            final_cost = entry_cost * 2
+        elif entry_cost > 701 and entry_cost <= 1500:
+            final_cost = entry_cost * 1.5
+        elif entry_cost > 1501 and entry_cost <= 3000:
+            calc = entry_cost * 0.85
+            final_cost = entry_cost + calc
+        elif entry_cost > 3001 and entry_cost <= 6000:
+            calc = entry_cost * 0.6
+            final_cost = entry_cost + calc
+        elif entry_cost > 6000:
+            calc = entry_cost * 0.45
+            final_cost = entry_cost + calc
+
+        serializer.save(final_cost=final_cost, code=biggestCode + 1)
 
 
 class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
